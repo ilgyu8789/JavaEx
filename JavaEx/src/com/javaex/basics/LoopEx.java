09d6aff0e71abadf93f55e8e1efc0e7e4542c2b4
ilgyu <t5nn123@naver.com>
2021-07-20 17:28:27 +0900

    반복문

--- JavaEx/src/com/javaex/basics/LoopEx.java
+++ JavaEx/src/com/javaex/basics/LoopEx.java
@@ -8,79 +8,63 @@ public class LoopEx {
 //		whileEx();
 //		whileGugu();
 //		doWhileEx();
-//		continueEx();
-//		Practice01Ex();
-		Practice02Ex();
-//		Practice03Ex();
+//		forGugu();
+		continueEx();
 		System.out.println("End of Code");
-	}
-
-	private static void Practice01Ex() {
-		// while문을 이용한 피라미드
-		String a = "*";
-		while(a.length() < 11) {
-			
-			System.out.println(a);
-				a = a + "*";		
-		}
-	}
-		
-	private static void Practice02Ex() {
-		// for문을 이용한 구구단
-	}
-	private static void Practice03Ex() {
-		// while문을 이용한 구구단 
-		Scanner sc = new Scanner(System.in);
-		int dan;
-		int i = 2;
-		
-		while (i < 10) {
-			int j = 1;
-			while (j  < 10) {
-				System.out.printf("%d * %d = %d\n", i, j, i * j);
-				j++;
-			}
-			i++;
-		}
 	}
 	
 	private static void continueEx() {
-		for (int i = 1; i <= 20; i++) {
-			// 2의 배수 3의 배수는 출력하지 않음 !
+		for (int i = 1;
+			i <= 20; 
+			i++) {
+			//	2의 배수, 3의 배수는 출력하지 않음
 			if (i % 2 == 0 || i % 3 == 0)
 				continue;
 			System.out.println(i);
 		}
+	}
+	
+	private static void forGugu() {
+		Scanner scanner = new Scanner(System.in);
 		
+		System.out.println("단을 입력하세요");
+		int dan = scanner.nextInt();
 		
+		for (int i = 1;	//	초기화
+			i <= 9;		//	반복 조건
+			i++) {		//	증감식
+			System.out.println(dan + "*" + i + "=" + (dan * i));
+		}
+		
+		scanner.close();
 	}
 	
 	private static void doWhileEx() {
 		//	적어도 한번은 수행되어야 하는 반복,
 		//	반복 조건이 반복문 내부에서 할당되는 경우
 		int total = 0, value = 0;
-
+		
 		Scanner scanner = new Scanner(System.in);
-
+		
 		do {
 			System.out.println("숫자를 입력하세요.[0이면 종료]");
 			value = scanner.nextInt();
 			total += value;	//	total = total + value
 		} while(value != 0);
-
+		
 		System.out.println("합계:" + total);
 		scanner.close();
 	}
-
+	
 	private static void whileGugu() {
 		Scanner scanner = new Scanner(System.in);
 		int dan;
 		int i = 1; // 반복 제어 변수
-
+		
 		System.out.println("단을 입력하세요");
 		System.out.print("단:");
 		dan = scanner.nextInt();
-
+		
 		while(i <= 9) {
 			System.out.println(
 					dan + "*" + i + "=" + (dan * i));
@@ -89,16 +73,15 @@ public class LoopEx {
 		}
 		scanner.close();
 	}
-
+	
 	private static void whileEx() {
 		int num = 0;	//	반복 조건 제어 변수
-
+		
 		while(num <= 9) {
 			System.out.println("I Like Java" + num);
 			num++;	//	주의: 무한 반복에 빠지지 않도록 적절히 제어
 		}
-
+		
 	}
 
 }
-// 구구단, *피라미드
