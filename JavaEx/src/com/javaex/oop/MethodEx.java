da24dbcc0eadeb91a28968ece6eec75dafdc65a9
ilgyu <t5nn123@naver.com>
2021-07-22 14:47:48 +0900

    Method, getter/setter 사용해서 접근제한자에 연결

---
+++ JavaEx/src/com/javaex/oop/MethodEx.java
@@ -0,0 +1,67 @@
+package com.javaex.oop;
+
+public class MethodEx {
+
+	public static void main(String[] args) {
+//		printMessage();
+		printMessage("Hello, Method");
+//		printDivide(10,0);
+//		System.out.println(divide(10, 3));
+		System.out.println("getSum:" + getSum(new double[] {
+				1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+		}));
+		System.out.println("getSumVar:" + 
+		getSumVar(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+		printSum("고정+가변", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+	}
+	
+	// 고정 인수가 앞에 가변 인수는 뒤에 나와야 함
+	private static void printSum(String message, double ...values) {
+		System.out.println(message + ":" + getSumVar(values));
+	}
+	
+	// 매개 변수의 갯수를 모른다
+	// 방법2. 가변 인수: 변수 이름 앞에 ...
+	private static double getSumVar(double ...values) {
+		// 가변인수는 배열로 변환되어 들어온다
+//		double total = 0;
+		
+//		for (double value: values) {
+//			total += value;
+//		}
+//		return total;
+		return getSum(values);
+	}
+	
+	// 방법1. 인수를 배열로 전달
+	private static double getSum(double[] values) {
+		double total = 0;
+		
+		for (double value: values) {
+			total += value;
+		}
+		return total;
+	}
+	
+	
+	//	입력o, 출력o
+	private static double divide(double num1, double num2) {
+		return num1 / num2;
+	}
+	
+	//	입력o, 출력x
+	private static void printDivide(int num1, int num2) {
+		if (num2 == 0) {
+			System.out.println("0으로 나눌 수 없어요!");
+			return;	//	돌아가세요
+		}
+		System.out.println();
+	}
+	// 입력o, 출력x
+	private static void printMessage(String message) {
+		System.out.println(message);
+	}
+	// 입력x, 출력x
+	
+	
+}
---
+++ JavaEx/src/com/javaex/oop/goods/v2/GoodsApp.java
@@ -0,0 +1,32 @@
+package com.javaex.oop.goods.v2;
+
+public class GoodsApp {
+
+	public static void main(String[] args) {
+		Goods notebook = new Goods();
+		// 필드에 접근
+//		notebook.name = "LG Gram";
+//		notebook.price = 1700000;
+		notebook.setName("LG Gram");
+		notebook.setPrice(1700000);
+		
+		Goods smartphone = new Goods();
+//		smartphone.name = "iPhone 12";
+//		smartphone.price = 800000;
+		smartphone.setName("iPhone 12");
+		smartphone.setPrice(800000);
+		
+		
+		//	출력
+		System.out.printf("%s, %d원%n",
+				notebook.getName(),
+				notebook.getPrice());
+		System.out.printf("%s, %d원%n",
+				smartphone.getName(),
+				smartphone.getPrice());
+		
+		notebook.showInfo();
+		smartphone.showInfo();
+	}
+
+}
