228e3c4febb98c8f9471554c85e7e4c1431dc852
ilgyu <t5nn123@naver.com>
2021-07-26 09:11:50 +0900

    출력 및 단축키

--- JavaEx/src/com/javaex/HelloWorld.java
+++ JavaEx/src/com/javaex/HelloWorld.java
@@ -4,5 +4,24 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// Show Message
 		System.out.println("Hello Java!");
+		System.out.println("Hello Java!");
+		System.out.println("Hello Java!");
+		System.out.println("Hello Java!");
 	}
 };
+
+
+//	ctrl + shift + L	단축키 전체 목록 보기
+//	ctrl + +, -		폰트 크기 증가/감소
+//	ctrl + D 	한 줄 삭제
+//	ctrl + alt + down(화살표키 아래방향)		행단위 복사
+//	alt + shift + A	멀티 컬럼 편집
+//	alt + up, down	행단위 이동
+//	ctrl + i	자동 들여쓰기
+//	ctrl + /	주석처리 -> 한줄 주석
+//	/* ~~ */	~~에 들어온 내용 모두 주석처리 -> 여러줄 주석
+//	ctrl + space	자동 완성 기능
+//	println(); -> 출력 후 줄바꿈
+//	print(); -> 출력 후 줄바꿈 안함
+//	("") -> ""가 있으면 ""안의 값을 그대로 출력
+//	() -> ""가 없으면 계산해서 출력
