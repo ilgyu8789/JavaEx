18ddefecd445cf29498d0c847d79f4e75389b4ef
ilgyu <t5nn123@naver.com>
2021-07-28 16:45:42 +0900

    List, Queue, Stack, Vector

---
+++ JavaEx/src/com/javaex/api/collection/list/ListEx.java
@@ -0,0 +1,52 @@
+package com.javaex.api.collection.list;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListEx {
+
+	public static void main(String[] args) {
+		// 다형성: 다른 코드에 손을 대지 않고 로직을 바꿀 수 있다
+//		List<String> lst = new LinkedList<>();
+		List<String> lst = new ArrayList<>();
+
+		// 객체 추가: add -> 맨 뒤에 새로운 요소 추가
+		lst.add("Java");
+		lst.add("C");
+		lst.add("C++");
+		lst.add("Python");
+
+		System.out.println("lst:" + lst);
+
+		lst.add("Java"); // Append
+
+		System.out.println("append:" + lst);
+		// List는 순서가 있고, 중복 요소를 허용한다
+
+		lst.add(3, "C#"); // Insert: 3번 인덱스에 C#을 삽입
+		System.out.println("insert:" + lst);
+
+		// 요소의 개수
+		System.out.println("size:" + lst.size());
+		// 버퍼 기반이 아니라 capacity는 없다
+		lst.remove(3);
+		System.out.println("remove:" + lst);
+		lst.remove("Python"); // 객체로 삭제
+		System.out.println("객체 삭제, remove" + lst);
+
+		// 반복자: List에서는 Iterator를 이용
+		Iterator<String> iter = lst.iterator();
+		while (iter.hasNext()) { // 뒤에 남은 요소가 있는가 ?
+			String item = iter.next(); // 요소를 받아오고 다음으로 이동
+			System.out.print(item + " ");
+		}
+		System.out.println();
+
+		// 리스트 비우기
+		lst.clear();
+		System.out.println(lst);
+	}
+
+}
---
+++ JavaEx/src/com/javaex/api/collection/list/QueueEx.java
@@ -0,0 +1,30 @@
+package com.javaex.api.collection.list;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueEx {
+
+	public static void main(String[] args) {
+		//	Queue: 선입선출(First Input First Output: FIFO)
+		Queue<Integer> queue = new LinkedList<>();
+		
+		//	데이터 제공
+		for(int i = 0; i < 5; i++) {
+			queue.offer(i);
+			System.out.println("QUEUE:" + queue);
+		}
+		
+		//	가장 먼저 입력된 데이터 확인 후 삭제는 안한다
+		System.out.println("PEEK:" + queue.peek());
+		System.out.println("QUEUE:" + queue);	//	0이 삭제되지 않고 출력
+		
+		//	poll 할 때는 반드시 비어있는지 확인
+		while(!queue.isEmpty()) {
+			System.out.println("POLL:" + queue.poll());
+			System.out.println("QUEUE:" + queue);
+		}
+		
+	}
+
+}
