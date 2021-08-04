565806c566ab23d381f0a6fc7977379dfc724402
ilgyu <t5nn123@naver.com>
2021-08-04 17:43:14 +0900

    Practice01_2 문제 6,7번

--- JavaEx/src/Practice01_2/Problem06.java
+++ JavaEx/src/Practice01_2/Problem06.java
@@ -1,5 +1,31 @@
 package Practice01_2;
 
-public class Problem06 {
+import java.util.*;
 
+public class Problem06 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		// 입력 받은 숫자가 홀수(짝수)인 경우 입력 값까지 홀수(짝수)의 합?
+		int even = 0;
+		int odd = 0;
+		
+		System.out.println("숫자 입력");
+		int s = scanner.nextInt();
+
+		for (int i = 0; i <= s; i++) {
+			if (i % 2 == 0) {
+				even = even + (i);
+			}
+		}
+		
+		for (int i = 0; i <= s; i++) {
+			if (i % 2 == 1) {
+				odd = odd + (i);
+			}
+		}
+		if(s%2==0)
+		System.out.println("1부터 10까지 짝수의 합 = " + even + "입니다.\n");
+		else if(s%2!=0)
+		System.out.println("1부터 10까지 홀수의 합 = " + odd + "입니다.\n");
+	}
 }
--- JavaEx/src/Practice01_2/Problem07.java
+++ JavaEx/src/Practice01_2/Problem07.java
@@ -1,5 +1,41 @@
 package Practice01_2;
 
+import java.util.*;
+
 public class Problem07 {
+	public static void main(String[] args) {
+		
+		int input = 0;
+		int answer = 0;
+
+		answer = (int) (Math.random() * 100) + 1;
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("====================");
+		System.out.println(" [숫자 맞추기 게임 시작]");
+		System.out.println("====================");
+		
+		do {
+			System.out.print(">>");
+			input = scanner.nextInt();
+
+			if (input > answer) {
+				System.out.println("더 작게");
+
+			} else if (input < answer) {
+				System.out.println("더 크게");
+
+			} else {
+				System.out.println("정답입니다");
+				System.out.println("게임을 종료하시겠습니까?<y/n>");
+			}
+
+		} while (input != answer);
+
+		
+		System.out.println("====================");
+		System.out.println(" [숫자 맞추기 게임 종료]");
+		System.out.println("====================");
+	}
 
 }
