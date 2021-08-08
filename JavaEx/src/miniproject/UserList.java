6366d635df36704bb837dc2dcb6ec2b3a9492642
박일규 <t5nn123@naver.com>
2021-08-09 01:56:10 +0900

    미니프로젝트

--- JavaEx/src/miniproject/UserList.java
+++ JavaEx/src/miniproject/UserList.java
@@ -1,28 +1,37 @@
 package miniproject;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class UserList {
 
+	ArrayList<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
+	
 	public static void List() {
-		
+		System.out.println("<1.리스트>");
+
 	}
 
 	public static void Enrollment() {
-		
+		System.out.println("<2.등록>");
 	}
 
 	public static void Delete() {
-		
+		System.out.println("<3.삭제>");
 	}
 
 	public static void Search() {
-		
+		System.out.println("<4.검색>");
 	}
 
 	public static void End() {
-		
+		System.out.println("******************");
+		System.out.println("*    감사합니다    *");
+		System.out.println("******************");
+
 	}
 
 	public static void Other() {
-		
+		System.out.println("[다시입력해주세요]");
 	}
 }
