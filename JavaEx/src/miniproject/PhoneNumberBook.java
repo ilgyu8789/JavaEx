ff4cacad075f815c455cd5b585f26993a98269f9
ilgyu <t5nn123@naver.com>
2021-08-10 09:19:25 +0900

    미니프젝수정

--- JavaEx/src/miniproject/PhoneNumberBook.java
+++ JavaEx/src/miniproject/PhoneNumberBook.java
@@ -6,29 +6,39 @@ import java.io.FileNotFoundException;
 
 public class PhoneNumberBook {
 
-	public static void PhoneBook() {
+	static File file = new File("./files/phonbook.txt");
 
-		ArrayList<String> name = new ArrayList<>();
-		ArrayList<String> hp = new ArrayList<>();
-		ArrayList<String> tel = new ArrayList<>();
-
+	public ArrayList<HashMap<String, String>> PhoneBook_read() {
+		ArrayList<HashMap<String, String>> pb_list = new ArrayList<HashMap<String, String>>();
 		try {
-			File file = new File("C:\\Users\\NDW\\git\\JavaEx\\files\\phonbook.txt");
-
 			Scanner scan = new Scanner(file);
+
+			int idx = 1;
+
 			while (scan.hasNextLine()) {
 
 				String[] split = scan.nextLine().split(", ");
 //				System.out.println(Arrays.toString(split));
-				name.add(split[0]);
-				hp.add(split[1]);
-				tel.add(split[2]);
-			}
 
+				HashMap<String, String> map = new HashMap<String, String>();
+				map.put("num", Integer.toString(idx));
+				map.put("name", split[0]);
+				map.put("hp", split[1]);
+				map.put("tel", split[2]);
+
+				pb_list.add(map);
+				idx++;
+			}
 			scan.close(); // try-catch를 쓸 때 scan은 try문 끝에서 닫아준다
 
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+		return pb_list;
 	}
+
+	public static void PhoneBook_write(String type) {
+
+	}
+
 }
