0074cb58260b52d59a776e10722c5a599ca98cd2
ilgyu <t5nn123@naver.com>
2021-08-10 18:27:30 +0900

    미니프젝

--- JavaEx/src/Practice01_2/Problem08.java
+++ JavaEx/src/Practice01_2/Problem08.java
@@ -1,5 +1,36 @@
 package Practice01_2;
 
-public class Problem08 {
+import java.util.*;
 
+public class Problem08 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		Random random = new Random(); // 랜덤 객체 생성
+		
+
+		int answer = 10; // random.nextInt(100); // 100까지의 수만 랜덤
+		System.out.println("====================");
+		System.out.println(" [숫자 맞추기 게임 시작]");
+		System.out.println("====================");
+
+		int input_number = 0;
+		while (true) {
+			do {
+				System.out.print(">> ");
+				input_number = scanner.nextInt();
+				if (input_number > answer) {
+					System.out.println("더 낮게");
+				}
+				if (input_number < answer) {
+					System.out.println("더 높게");
+				}
+			} while (input_number != answer);
+			
+			System.out.println("정답입니다");
+			System.out.printf("게임을 종료하시겠습니까?(y/n)%n");
+			System.out.println("====================");
+			System.out.println(" [숫자 맞추기 게임 종료]");
+			System.out.println("====================");
+		}
+	}
 }
---
+++ JavaEx/src/com/javaex/jdbc/oracle/SelectTest.java
@@ -0,0 +1,51 @@
+package com.javaex.jdbc.oracle;
+
+import java.sql.*;
+
+public class SelectTest {
+
+	public static void main(String[] args) {
+		String dburl = "jdbc:oracle:thin:@localhost:1521:xe";
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			//	드라이버 로드
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			//	Connection
+			conn = DriverManager.getConnection(dburl, "HR", "hr");
+			//	질의 객체(Statement)
+			stmt = conn.createStatement();
+			//	SQL 설정
+			String sql = "SELECT department_id, department_name" +
+						" FROM departments ORDER BY department_id";
+			//	실행
+			rs = stmt.executeQuery(sql);
+			
+			//System.out.println(rs);
+			//	루프 후 출력
+			while (rs.next()) { //	다음 레코드 가져오기
+				int deptId = rs.getInt(1);	//	컬럼 순서로 가져오기
+				String deptName = rs.getString("department_name");
+				//	컬럼 이름으로 가져오기
+				System.out.printf("%d:%s%n", deptId, deptName);
+				
+			}
+		} catch (ClassNotFoundException e) {
+			System.err.println("드라이버 로드 실패!");
+		} catch (SQLException e) {
+			System.err.println("SQL Error!");
+		} finally {
+			try {
+				rs.close();
+				stmt.close();
+				conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+}
